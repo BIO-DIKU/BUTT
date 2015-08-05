c207770da5b5b6a630ffebc3c133768a1bcd0d62
Martin A. Hansen <mail@maasha.dk>
2015-08-05 21:14:36 +0200

    rewrite ala cpplint

--- BUTT/main.cpp
+++ BUTT/main.cpp
@@ -1,3 +1,5 @@
+/* Copyright 2015 BIO-DIKU */
+
 #include <iostream>
 #include <string>
 
@@ -9,29 +11,26 @@ using namespace std;
 
 void benchmarkBuilder(int argc, char** argv);
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     TestAll tests;
     tests.runTests();
 
-    //benchmarkBuilder(argc, argv);
+    // benchmarkBuilder(argc, argv);
 
     return 0;
 }
 
-
-void benchmarkBuilder(int argc, char** argv)
-{
-    cout<<"Assuming second argument is a fasta file"<<endl;
+void benchmarkBuilder(int argc, char** argv) {
+    cout << "Assuming second argument is a fasta file" << endl;
 
     Fasta fasta(argv[1]);
     TaxBuilder builder(SeqToKMers(9, 1));
 
-    while(fasta.hasNextEntry()){
+    while (fasta.hasNextEntry()) {
         FastaEntry entry = fasta.nextEntry();
 
-        string taxString = entry.header.substr( entry.header.find(' ')+1 );
-        builder.addTaxEntry( taxString, entry.sequence );
+        string taxString = entry.header.substr(entry.header.find(' ') + 1);
+        builder.addTaxEntry(taxString, entry.sequence);
     }
 
     string kmerFile("kmerIndex.txt");
@@ -39,5 +38,4 @@ void benchmarkBuilder(int argc, char** argv)
 
     string taxFile("taxIndex.txt");
     builder.saveTaxIndex(taxFile);
-
 }
