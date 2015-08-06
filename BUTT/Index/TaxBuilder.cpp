e5152bb7f2b7e7fb311e57b716d9d08de73b1e69
Martin A. Hansen <mail@maasha.dk>
2015-08-06 13:47:22 +0200

    rewrite ala cpplint

--- BUTT/Index/TaxBuilder.cpp
+++ BUTT/Index/TaxBuilder.cpp
@@ -1,7 +1,11 @@
+/* Copyright 2015 BIO-DIKU */
+
 #include <vector>
 #include <string>
 #include <iostream>
 #include <list>
+#include <map>
+#include <set>
 
 #include "TaxBuilder.h"
 #include "Common.h"
@@ -9,165 +13,181 @@
 TaxBuilder::TaxBuilder(SeqToKMers seqSplitter):
     sequenceSplitter(seqSplitter),
     root(new TaxNode()),
-    tree_size(1)
-{
+    tree_size(1) {
 }
 
-TaxBuilder::~TaxBuilder()
-{
+TaxBuilder::~TaxBuilder() {
     delete root;
 }
 
-void TaxBuilder::addTaxEntry(std::string &&taxonomyString, std::string &&sequence)
-{
+void TaxBuilder::addTaxEntry(std::string &&taxonomyString,
+                             std::string &&sequence) {
     addTaxEntry(taxonomyString, sequence);
 }
 
-void TaxBuilder::addTaxEntry(std::string &taxonomyString, std::string &sequence)
-{
+void TaxBuilder::addTaxEntry(std::string &taxonomyString,
+                             std::string &sequence) {
     std::vector<std::string> tax_tokens;
     bool tax_string_status = checkTaxEntry(taxonomyString, tax_tokens);
 
-    if(!tax_string_status) {
-        std::string msg = "Taxonomy string not valid: "+taxonomyString;
+    if (!tax_string_status) {
+        std::string msg = "Taxonomy string not valid: " + taxonomyString;
         throw TaxonomyStringException(msg);
     }
 
     TaxNode* node = root;
 
-    for(unsigned int t=0;t<tax_tokens.size();t++)
-    {
+    for (unsigned int t = 0; t < tax_tokens.size(); t++) {
         TaxNode* next_child = node->getChild(tax_tokens[t]);
-        if(next_child==NULL)
-            next_child = new TaxNode(node, tax_tokens[t], tree_size++);
 
-        if(isLeaf(tax_tokens, t)){
-            //next_child->addKMers(sequenceSplitter.sequenceToKMers(sequence));
+        if (next_child == NULL) {
+            next_child = new TaxNode(node, tax_tokens[t], tree_size++);
+        }
+
+        if (isLeaf(tax_tokens, t)) {
+            // next_child->addKMers(sequenceSplitter.sequenceToKMers(sequence));
             sequenceSplitter.addKMersToSet(sequence, next_child->kmers);
             break;
-        }else{
+        } else {
             node = next_child;
         }
     }
-
 }
 
-bool TaxBuilder::isLeaf(std::vector<std::string> &tax_path, unsigned int t)
-{
-    return (t+1>=tax_path.size() || tax_path[t+1].length()==2); // leaf if tax_path[t+1] has format "C#"
+bool TaxBuilder::isLeaf(std::vector<std::string> &tax_path, unsigned int t) {
+    // leaf if tax_path[t+1] has format "C#"
+    return (t + 1 >= tax_path.size() || tax_path[t + 1].length() == 2);
 }
 
-bool TaxBuilder::checkTaxEntry(std::string &taxonomyString, std::vector<std::string> &splitTaxString)
-{
+bool TaxBuilder::checkTaxEntry(std::string &taxonomyString,
+                               std::vector<std::string> &splitTaxString) {
     split(taxonomyString, ';', splitTaxString);
 
-    if(level_names.empty()){ // If this is the first taxonomyString that is checked, fill up level_names
-        for(int i=0;i<splitTaxString.size();i++){
-            if(splitTaxString[i].length()<2 || splitTaxString[i][1]!='#') return false; //Check criteria 1
-            if(i<splitTaxString.size()-1 && splitTaxString[i].length()==2 && splitTaxString[i+1].length()>2)
-                return false; //Check criteria 3
+    if (level_names.empty()) {
+        // If this is the first taxonomyString that is checked, fill up
+        // level_names.
+        for (int i = 0; i < splitTaxString.size(); i++) {
+            if (splitTaxString[i].length() < 2 || splitTaxString[i][1] != '#') {
+                return false;   // Check criteria 1
+            }
+
+            if (i < splitTaxString.size() - 1 && splitTaxString[i].length() == 2
+                && splitTaxString[i + 1].length() > 2) {
+                return false;   // Check criteria 3
+            }
 
             char level_name = splitTaxString[i][0];
 
-            if(std::find(level_names.begin(), level_names.end(), level_name)!=level_names.end())
-                return false; // Check criteria 2
+            if (std::find(level_names.begin(), level_names.end(), level_name) !=
+                level_names.end()) {
+                return false;   // Check criteria 2
+            }
 
             level_names.push_back(level_name);
         }
 
-    }else{ // Any taxonomyString but the first have to match the first
+    } else {   // Any taxonomyString but the first have to match the first
+        if (splitTaxString.size() != level_names.size()) {
+            return false;   // Check criteria 4
+        }
 
-        if(splitTaxString.size()!=level_names.size()) return false; //Check criteria 4
+        for (int i = 0; i < splitTaxString.size(); i++) {
+            if (splitTaxString[i].length() < 2 || splitTaxString[i][1] != '#') {
+                return false;   // Check criteria 1
+            }
 
-        for(int i=0;i<splitTaxString.size();i++){
-            if(splitTaxString[i].length()<2 || splitTaxString[i][1]!='#') return false; //Check criteria 1
-            if(i<splitTaxString.size()-1 && splitTaxString[i].length()==2 && splitTaxString[i+1].length()>2)
-                return false; //Check criteria 3
+            if (i < splitTaxString.size() - 1 && splitTaxString[i].length() == 2
+                && splitTaxString[i+1].length() > 2) {
+                return false;   // Check criteria 3
+            }
 
             char level_name = splitTaxString[i][0];
 
-            if(level_names[i]!=level_name) return false; //Check criteria 4 and implicitly also 2
+            if (level_names[i] != level_name) {
+                return false;   // Check criteria 4 and implicitly also 2
+            }
         }
-
     }
 
     return true;
 }
 
-TaxNode& TaxBuilder::getNode(unsigned int id)
-{
+TaxNode& TaxBuilder::getNode(unsigned int id) {
     TaxNode* n = root->findNode(id);
     return *n;
 }
 
-unsigned int TaxBuilder::treeSize()
-{
+unsigned int TaxBuilder::treeSize() {
     return tree_size;
 }
 
-void TaxBuilder::pullUnions(TaxNode &n)
-{
-    if(n.children.size()==0)
-        return;
+void TaxBuilder::pullUnions(TaxNode &n) {
+    if (n.children.size() == 0) return;
 
-    for(auto child_it=n.children.begin(); child_it!=n.children.end(); child_it++){
-        pullUnions( *(child_it->second) );
+    for (auto child_it = n.children.begin(); child_it != n.children.end();
+         child_it++) {
+        pullUnions(*(child_it->second));
     }
 
-    for(auto child_it=n.children.begin(); child_it!=n.children.end(); child_it++){
+    for (auto child_it = n.children.begin(); child_it != n.children.end();
+         child_it++) {
         n.addKMers(child_it->second->kmers);
     }
 }
 
-void TaxBuilder::saveTaxIndex(TaxNode* n, int depth, std::ofstream &ostream)
-{
-    int parent_id = (depth==0?-1:n->getParent().node_id);
-    ostream<<n->node_id<<'\t'<<parent_id<<'\t'<<depth<<'\t'<<n->getName()<<std::endl;
+void TaxBuilder::saveTaxIndex(TaxNode* n, int depth, std::ofstream &ostream) {
+    int parent_id = (depth == 0 ? -1 : n->getParent().node_id);
+    ostream << n->node_id << '\t' << parent_id << '\t' << depth << '\t' <<
+               n->getName() << std::endl;
 
-    for(auto child_it=n->children.begin(); child_it!=n->children.end(); child_it++){
-        saveTaxIndex(child_it->second, depth+1, ostream);
+    for (auto child_it = n->children.begin(); child_it != n->children.end();
+         child_it++) {
+        saveTaxIndex(child_it->second, depth + 1, ostream);
     }
 }
 
-
-void TaxBuilder::saveTaxIndex(std::string &file_path)
-{
+void TaxBuilder::saveTaxIndex(std::string &file_path) {
     std::ofstream ostream(file_path);
-    ostream<<"#NODE_ID\tPARENT_ID\tLEVEL\tNAME"<<std::endl;
+    ostream << "#NODE_ID\tPARENT_ID\tLEVEL\tNAME" << std::endl;
     saveTaxIndex(root, 0, ostream);
     ostream.close();
 }
 
-void TaxBuilder::buildKMerIndex(TaxNode* n, int depth, std::vector< std::map< KMer, std::list<int> > > &kmer_index)
-{
-    while(kmer_index.size()<=depth)
+void TaxBuilder::buildKMerIndex(TaxNode* n, int depth,
+                                std::vector< std::map< KMer,
+                                std::list<int> > > &kmer_index) {
+    while (kmer_index.size() <= depth) {
         kmer_index.push_back(std::map<KMer, std::list<int> >());
+    }
 
     KMerSet &kmers = n->getKMers();
-    for(auto kmer_it=kmers.begin(); kmer_it!=kmers.end(); kmer_it++){
+
+    for (auto kmer_it = kmers.begin(); kmer_it != kmers.end(); kmer_it++) {
         KMer kmer = *kmer_it;
         kmer_index[depth][kmer].push_back(n->node_id);
     }
 
-    for(auto child_it=n->children.begin(); child_it!=n->children.end(); child_it++){
-        buildKMerIndex(child_it->second, depth+1, kmer_index);
+    for (auto child_it = n->children.begin(); child_it != n->children.end();
+         child_it++) {
+        buildKMerIndex(child_it->second, depth + 1, kmer_index);
     }
 }
 
-void TaxBuilder::saveKMerIndex(std::string &file_path)
-{
+void TaxBuilder::saveKMerIndex(std::string &file_path) {
     pullUnions(*root);
 
     std::ofstream ostream(file_path);
-    ostream<<"#LEVEL\tKMER\tNODES"<<std::endl;
+    ostream << "#LEVEL\tKMER\tNODES" << std::endl;
 
-    //Perform a standard BFS following http://en.wikipedia.org/wiki/Breadth-first_search
+    // Perform a standard BFS following
+    // http://en.wikipedia.org/wiki/Breadth-first_search
     std::list< TaxNode* >* queue1 = new std::list<TaxNode*>();
     std::list< TaxNode* >* queue2 = new std::list<TaxNode*>();
     queue2->push_back(root);
 
     int lvl = -1;
-    while(!queue2->empty()){
+
+    while (!queue2->empty()) {
         lvl++;
 
         // Swap queues
@@ -177,39 +197,42 @@ void TaxBuilder::saveKMerIndex(std::string &file_path)
 
         // Collect KMer-to-nodeid map
         std::map< KMer, std::set<int> > kmer_map;
-        while(!queue1->empty()){
-            TaxNode* n = queue1->front(); queue1->pop_front();
 
-            KMerSet &kmers = n->getKMers();
-            for(auto kmer_it=kmers.begin(); kmer_it!=kmers.end(); kmer_it++){
+        while (!queue1->empty()) {
+            TaxNode*  n     = queue1->front(); queue1->pop_front();
+            KMerSet  &kmers = n->getKMers();
+
+            for (auto kmer_it = kmers.begin(); kmer_it != kmers.end();
+                 kmer_it++) {
                 KMer kmer = *kmer_it;
                 kmer_map[kmer].insert(n->node_id);
             }
 
-            //Put children of n in queue2
-            for(auto child_it=n->children.begin(); child_it!=n->children.end(); child_it++){
+            // Put children of n in queue2
+            for (auto child_it = n->children.begin();
+                 child_it != n->children.end(); child_it++) {
                 queue2->push_back(child_it->second);
             }
         }
 
         // Print
-        for(auto kmer_it = kmer_map.begin(); kmer_it!=kmer_map.end(); kmer_it++){
-            ostream<<lvl<<'\t'<<kmer_it->first<<'\t';
-            for(auto node_it=kmer_it->second.begin(); node_it!=kmer_it->second.end(); node_it++){
-                ostream<<*node_it<<';';
-            }
-            ostream<<std::endl;
-        }
+        for (auto kmer_it = kmer_map.begin(); kmer_it != kmer_map.end();
+             kmer_it++) {
+            ostream << lvl << '\t' << kmer_it->first << '\t';
 
+            for (auto node_it = kmer_it->second.begin();
+                 node_it != kmer_it->second.end(); node_it++) {
+                ostream << *node_it << ';';
+            }
+
+            ostream << std::endl;
+        }
     }
 
     delete queue1;
     delete queue2;
 
-
     ostream.close();
-
-
 
     /*
     std::vector< std::map< KMer, std::list<int> > > kmer_index;
@@ -229,6 +252,4 @@ void TaxBuilder::saveKMerIndex(std::string &file_path)
     }
     ostream.close();
     */
-
-
 }
--- BUTT/Index/TaxBuilder.h
+++ BUTT/Index/TaxBuilder.h
@@ -1,9 +1,13 @@
-#ifndef TAXBUILDER_H
-#define TAXBUILDER_H
+/* Copyright 2015 BIO-DIKU */
+
+#ifndef BUTT_INDEX_TAXBUILDER_H_
+#define BUTT_INDEX_TAXBUILDER_H_
 
 #include <string>
 #include <list>
 #include <fstream>
+#include <vector>
+#include <map>
 
 #include "SeqToKMers.h"
 #include "TaxNode.h"
@@ -13,23 +17,23 @@
  * @brief Exception thrown by TaxBuilder class
  */
 class TaxonomyStringException: public std::exception {
-public:
-    TaxonomyStringException(std::string &msg): exceptionMsg(msg){}
-    TaxonomyStringException(const TaxonomyStringException &e): exceptionMsg(e.exceptionMsg){}
+ public:
+    TaxonomyStringException(std::string &msg): exceptionMsg(msg) {}
+    TaxonomyStringException(const TaxonomyStringException &e):
+        exceptionMsg(e.exceptionMsg) {}
     const std::string exceptionMsg;
 };
 
 
 /**
  * @brief The TaxBuilder class
- * Class for building the index for the taxonomy classifier. After construction a
- * set of fasta entries can be added and as a final step the index can be generated
- * with the saveIndex function. Only entries added before a call to saveIndex
- * are stored.
+ * Class for building the index for the taxonomy classifier. After construction
+ * a set of fasta entries can be added and as a final step the index can be
+ * generated with the saveIndex function. Only entries added before a call to
+ * saveIndex are stored.
  */
-class TaxBuilder
-{
-public:
+class TaxBuilder {
+ public:
     /**
      * @brief TaxBuilder
      * Prepare the taxonomy builder. No real action is taken here.
@@ -40,13 +44,15 @@ public:
     ~TaxBuilder();
 
     /**
-     * @brief Add a taxonomy entry specified by a taxonomy string and a sequence to the
-     * taxonomy classifier.
-     * A TaxonomyStringException is thrown if the taxonomy string is malformated.
-     * @param taxonomyString A taxonomy string of the format "[<C>#<Name>;<C>#<Name>;..]"
-     * where <C> is a classification character indicating the level of the classification
-     * and <Name> is the classification itself. An example of a valid taxonomy string could be:
-     * "K#Bacteria;P#Proteobacteria;C#Gammaproteobacteria;O#Vibrionales;F#Vibrionaceae;G#Vibrio;S#Vibrio"
+     * @brief Add a taxonomy entry specified by a taxonomy string and a sequence
+     * to the taxonomy classifier. A TaxonomyStringException is thrown if the
+     * taxonomy string is malformated.
+     * @param taxonomyString A taxonomy string of the format
+     * "[<C>#<Name>;<C>#<Name>;..]" where <C> is a classification character
+     * indicating the level of the classification and <Name> is the
+     * classification itself. An example of a valid taxonomy string could be:
+     * "K#Bacteria;P#Proteobacteria;C#Gammaproteobacteria;O#Vibrionales; \
+     * F#Vibrionaceae;G#Vibrio;S#Vibrio"
      * @param sequence An RNA/DNA sequence associated with this taxonomy.
      */
     void addTaxEntry(std::string &&taxonomyString, std::string &&sequence);
@@ -63,25 +69,27 @@ public:
      * 37426      0            1          K#Archaea
      * 37427      37426        2          P#Crenarchaeota
      * 37428      37427        3          C#Thermoprotei
-     * The NODE_ID is a unique non-negative identifier for tax-string nodes created using a
-     * depth-first traversal of the taxonomy tree. The PARENT_ID is the NODE_ID of the parent
-     * except for the root node where the PARENT_ID is -1. LEVEL is the depth of the node
-     * in the taxonomy tree and NAME is either 'root' or the specific part of the taxonomy
-     * string related to the nodes level.
+     * The NODE_ID is a unique non-negative identifier for tax-string nodes
+     * created using a depth-first traversal of the taxonomy tree. The
+     * PARENT_ID is the NODE_ID of the parent except for the root node where
+     * the PARENT_ID is -1. LEVEL is the depth of the node in the taxonomy tree
+     * and NAME is either 'root' or the specific part of the taxonomy string
+     * related to the nodes level.
      */
     void saveTaxIndex(std::string &file_path);
 
     /**
      * @brief Build and save kmer index.
-     * The kmer index is a clear-text file that for each level indicates for each kmer the
-     * node ids containing this kmer. The file is formatted using 3 columns where the first
-     * indicates the LEVEL, the second indicates the KMER, and the third is a semicolon
-     * separated sorted list of NODES (node-ids) that contain KMER.
+     * The kmer index is a clear-text file that for each level indicates for
+     * each kmer the node ids containing this kmer. The file is formatted using
+     * 3 columns where the first indicates the LEVEL, the second indicates the
+     * KMER, and the third is a semicolon separated sorted list of NODES
+     * (node-ids) that contain KMER.
      */
     void saveKMerIndex(std::string &file_path);
 
     /**
-     * @brief Return the node given an id.
+     * gg@brief Return the node given an id.
      * Throws a MissingNodeException if unknown id.
      * This function is provided only for debugging and testing purposes.
      */
@@ -92,8 +100,8 @@ public:
      * This function is provided only for debugging and testing purposes.
      */
     unsigned int treeSize();
-private:
 
+ private:
     const SeqToKMers sequenceSplitter;
 
     TaxNode* root;
@@ -105,27 +113,33 @@ private:
 
     /**
      * @brief checkTaxEntry checks if the taxonomyString satisfies:
-     * 1: The format must be C#Name;C#Name;...C#Name where C can be any single character and Name is a (possibly empty) string without '#'
+     * 1: The format must be C#Name;C#Name;...C#Name where C can be any single
+     *    character and Name is a (possibly empty) string without '#'
      * 2: Each level has its own distinct character
      * 3: If a level has an empty name all following names must be empty
-     * 4: The number and order of level names is the same as for any previous checked entry
-     * @param taxonomyString will be filled with the taxonomyString split by ';' for efficiency
+     * 4: The number and order of level names is the same as for any previous
+     * checked entry
+     * @param taxonomyString will be filled with the taxonomyString split by
+     * ';' for efficiency
      * @return true iff the taxonomyString satisfies the above criteria
      */
-    bool checkTaxEntry(std::string &taxonomyString, std::vector<std::string> &splitTaxString);
+    bool checkTaxEntry(std::string &taxonomyString,
+                       std::vector<std::string> &splitTaxString);
 
     std::vector<char> level_names;
 
     friend class TestTaxBuilder;
 
     /**
-     * When fasta entries are added their k-mers are only added to the bottom node in
-     * their classification. This function pulls all k-mers into parent-nodes using unions.
+     * When fasta entries are added their k-mers are only added to the bottom
+     * node in their classification. This function pulls all k-mers into
+     * parent-nodes using unions.
      */
     void pullUnions(TaxNode &n);
 
     void saveTaxIndex(TaxNode* n, int depth, std::ofstream &ostream);
-    void buildKMerIndex(TaxNode* n, int depth, std::vector< std::map< KMer, std::list<int> > > &kmer_index);
+    void buildKMerIndex(TaxNode* n, int depth, std::vector< std::map< KMer,
+                        std::list<int> > > &kmer_index);
 };
 
-#endif // TAXBUILDER_H
+#endif   // BUTT_INDEX_TAXBUILDER_H_
