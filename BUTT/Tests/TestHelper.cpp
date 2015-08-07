072b480b71c1b108d86b1d19f1a8960501e3aa0c
Martin A. Hansen <mail@maasha.dk>
2015-08-07 15:47:50 +0200

    rewrite ala cpplint

--- BUTT/Tests/TestHelper.cpp
+++ BUTT/Tests/TestHelper.cpp
@@ -1,41 +1,43 @@
+/* Copyright 2015 BIO-DIKU */
+
 #include "TestHelper.h"
 
+#include <string>
 #include <iostream>
 
+bool BUTTTest::assertEquals(const std::string &expected,
+                            const std::string &actual, std::string message) {
+    if (expected.compare(actual) != 0) {
+        std::cerr << "Error: " << message << std::endl;
+        return false;
+    }
 
-bool BUTTTest::assertEquals(const std::string &expected, const std::string &actual, std::string message)
-{
-    if(expected.compare(actual)!=0) {
-        std::cerr<<"Error: "<<message<<std::endl;
-        return false;
-    }
-    return true;
-}
-bool BUTTTest::assertEquals(int expected, int actual, std::string message)
-{
-    if(expected!=actual){
-        std::cerr<<"Error: "<<message<<std::endl;
-        return false;
-    }
     return true;
 }
 
-bool BUTTTest::assertTrue(const bool &actual, std::string message)
-{
-    if(!actual) {
-        std::cerr<<"Error: "<<message<<std::endl;
+bool BUTTTest::assertEquals(int expected, int actual, std::string message) {
+    if (expected != actual) {
+        std::cerr << "Error: " << message << std::endl;
         return false;
     }
+
     return true;
 }
 
-bool BUTTTest::assertFalse(const bool &actual, std::string message)
-{
-    if(actual) {
-        std::cerr<<"Error: "<<message<<std::endl;
+bool BUTTTest::assertTrue(const bool &actual, std::string message) {
+    if (!actual) {
+        std::cerr << "Error: " << message << std::endl;
         return false;
     }
+
     return true;
 }
 
+bool BUTTTest::assertFalse(const bool &actual, std::string message) {
+    if (actual) {
+        std::cerr << "Error: " << message << std::endl;
+        return false;
+    }
 
+    return true;
+}
--- BUTT/Tests/TestHelper.h
+++ BUTT/Tests/TestHelper.h
@@ -1,23 +1,32 @@
-#ifndef TESTHELPER_H
-#define TESTHELPER_H
+/* Copyright 2015 BIO-DIKU */
+
+#ifndef BUTT_TESTS_TESTHELPER_H_
+#define BUTT_TESTS_TESTHELPER_H_
 
 #include <string>
 
-namespace BUTTTest{
-    bool assertEquals(const std::string &expected, const std::string &actual, std::string message);
+namespace BUTTTest {
+    bool assertEquals(const std::string &expected, const std::string &actual,
+                      std::string message);
     bool assertEquals(int expected, int actual, std::string message);
     bool assertTrue(const bool &actual, std::string message);
     bool assertFalse(const bool &actual, std::string message);
 }
 
-#define BUTT_ASSERT_EQUALS(expected, actual, message) if(!BUTTTest::assertEquals(expected,actual,message)) return false;
-#define BUTT_ASSERT_TRUE(actual, message) if(!BUTTTest::assertTrue(actual,message)) return false;
-#define BUTT_ASSERT_FALSE(actual, message) if(!BUTTTest::assertFalse(actual,message)) return false;
+#define BUTT_ASSERT_EQUALS(expected, actual, message) \
+    if (!BUTTTest::assertEquals(expected, actual, message)) return false;
 
-#define BUTT_PRE_TESTS() bool status=true; bool testStatus;
+#define BUTT_ASSERT_TRUE(actual, message) \
+    if (!BUTTTest::assertTrue(actual, message)) return false;
+
+#define BUTT_ASSERT_FALSE(actual, message) \
+    if (!BUTTTest::assertFalse(actual, message)) return false;
+
+#define BUTT_PRE_TESTS() bool status = true; bool testStatus;
 #define BUTT_POST_TESTS() return status;
 #define BUTT_RUN_TEST(message, function) \
     testStatus = function; \
     status&=testStatus; \
-    std::cerr<<message<<": "<<(testStatus?"successful":"failed")<<std::endl;
-#endif // TESTHELPER_H
+    std::cerr << message << ": " << (testStatus ? "successful" : "failed") \
+              << std::endl;
+#endif   // BUTT_TESTS_TESTHELPER_H_
