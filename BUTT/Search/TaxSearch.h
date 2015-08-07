9cab7531c4a10f2279ea6acb68e454ceef2b78a4
Martin A. Hansen <mail@maasha.dk>
2015-08-07 10:15:33 +0200

    rewrite ale cpplint

--- BUTT/Search/TaxSearch.h
+++ BUTT/Search/TaxSearch.h
@@ -1,5 +1,7 @@
-#ifndef TAXSEARCH_H
-#define TAXSEARCH_H
+/* Copyright 2015 BIO-DIKU */
+
+#ifndef BUTT_SEARCH_TAXSEARCH_H_
+#define BUTT_SEARCH_TAXSEARCH_H_
 
 #include <tuple>
 #include <set>
@@ -7,6 +9,7 @@
 #include <vector>
 #include <list>
 #include <utility>
+
 #include "Common.h"
 #include "Search/NameNode.h"
 #include "Search/TaxConsensus.h"
@@ -17,9 +20,10 @@
  * @brief Exception thrown by TaxSearch class
  */
 class TaxSearchException: public std::exception {
-public:
-    TaxSearchException(std::string &&msg): exceptionMsg(msg){}
-    TaxSearchException(const TaxSearchException &e): exceptionMsg(e.exceptionMsg){}
+ public:
+    TaxSearchException(std::string &&msg): exceptionMsg(msg) {}
+    TaxSearchException(const TaxSearchException &e):
+        exceptionMsg(e.exceptionMsg) {}
     const std::string exceptionMsg;
 };
 
@@ -35,22 +39,23 @@ public:
 typedef std::tuple<std::string, std::string, int> Hit;
 std::ostream & operator<<(std::ostream & os, Hit hit);
 
-class TaxSearch
-{
-public:
+class TaxSearch {
+ public:
     /**
      * @brief TaxSearch
      * @param seqSplitter
      * @param hits_max Include maximally this number of hits in the consensus.
      * @param best_only Include only the best scoring hits in the consensus.
-     * That is if a hit consists of 344 kmers out of 345 possible, only hits with
-     * 344 kmers are included.
+     * That is if a hit consists of 344 kmers out of 345 possible, only hits
+     * with 344 kmers are included.
      * @param coverage Filter hits based on kmer coverage. If a hit contains
      * @param kmer_index_path File path for the kmer index file
-     * @param tax_index_path File path for the taxonomy index file
-     * fewer kmers than the total amount of kmers x coverage it will be filtered.
+     * @param tax_index_path File path for the taxonomy index file fewer kmers
+     * than the total amount of kmers x coverage it will be filtered.
      */
-    TaxSearch(SeqToKMers seqSplitter, int hits_max, bool best_only, double coverage, TaxConsensus* consensus_builder, std::string &kmer_index_path, std::string &tax_index_path);
+    TaxSearch(SeqToKMers seqSplitter, int hits_max, bool best_only,
+              double coverage, TaxConsensus* consensus_builder,
+              std::string &kmer_index_path, std::string &tax_index_path);
 
     /**
      * @brief Perform a BUTT search using searchNodes and compile a consensus
@@ -60,7 +65,8 @@ public:
      *
      * @return a tuple containing the seqName and consensus string.
      * Example:
-     * ("Query 1","K#Bacteria(100);P#Proteobacteria(100);C#Gammaproteobacteria(80);O#Vibrionales(75);F#;G#;S#")
+     * ("Query 1","K#Bacteria(100);P#Proteobacteria(100); \
+     *  C#Gammaproteobacteria(80);O#Vibrionales(75);F#;G#;S#")
      */
     Hit search(std::string &seqName, std::string &sequence);
 
@@ -68,12 +74,12 @@ public:
     Hit search(std::string &&seqName, std::string &&sequence);
 
     /**
-     * @brief Method to search for a given sequence.
-     * First the sequence is broken down into unique k-mers using the
-     * seqSplitter.  For each taxonomic level starting from the deepest, all
-     * nodes containing these k-mers are looked up. The nodes containing at
-     * least a ratio of TaxSearch::coverage of the k-mers are considered node-hits.
-     * If there are no node-hits at a taxonomic level, we move to the next level.
+     * @brief Method to search for a given sequence. First the sequence is
+     * broken down into unique k-mers using the seqSplitter.  For each
+     * taxonomic level starting from the deepest, all nodes containing these
+     * k-mers are looked up. The nodes containing at least a ratio of
+     * TaxSearch::coverage of the k-mers are considered node-hits. If there are
+     * no node-hits at a taxonomic level, we move to the next level.
      *
      * If TaxSearch::best_only is set, only the (at most TaxSearch::hits_max)
      * node-hits with maximum coverage are returned. Otherwise the (at most
@@ -88,7 +94,7 @@ public:
      */
     std::vector<int> searchNodes(std::string &sequence);
 
-private:
+ private:
     const SeqToKMers seq_splitter;
     const int hits_max;
     const bool best_only;
@@ -97,38 +103,43 @@ private:
     std::vector<NameNode> nodes;
     std::vector< std::string > level_names;
 
-    /// Vector with levels.
+    // Vector with levels.
     std::vector< TaxLevel > level_vector;
 
-    /// Working memory for building histograms in search. Arrays are used in the hope
-    /// that memset will be faster than vector::assign.
+    // Working memory for building histograms in search. Arrays are used in the
+    // hope that memset will be faster than vector::assign.
     std::vector<unsigned int> node_counts;
 
     /**
-     * @brief Reads the taxonomy index files into data structure and prepare for the
-     * search. If the file does not exist, a FileNotFoundException will be raised.
+     * @brief Reads the taxonomy index files into data structure and prepare
+     * for the search. If the file does not exist, a FileNotFoundException will
+     * be raised.
      * @param file_path
      */
     void readTaxIndex(std::string &file_path);
 
     /**
-     * @brief Reads the kmer index files into data structure and prepare for the
-     * search. If the file does not exist, a FileNotFoundException will be raised.
+     * @brief Reads the kmer index files into data structure and prepare for
+     * the search. If the file does not exist, a FileNotFoundException will be
+     * raised.
      * @param file_path
      */
     void readKMerIndex(std::string &file_path);
 
     /**
-     * @brief Given a node ID find the node in the taxonomy tree and collect 
+     * @brief Given a node ID find the node in the taxonomy tree and collect
      * node names from the node ID to the root.
      * @param node_id
      * @param node_tax_row Vector for returning names.
      */
     void fill_node_tax_row(int node_id, std::vector<std::string> &node_tax_row);
 
-    static bool descendingSortOrder(unsigned int a, unsigned int b){ return a > b; }
+    static bool descendingSortOrder(unsigned int a, unsigned int b) {
+        return a > b;
+    }
 
-    static bool descendingPairSortOrder(const std::pair<int, int> &a, const std::pair<int, int> &b) {
+    static bool descendingPairSortOrder(const std::pair<int, int> &a,
+                                        const std::pair<int, int> &b) {
         return a.second > b.second;
     }
 
@@ -137,4 +148,4 @@ private:
     void readTaxLevelNames();
 };
 
-#endif // TAXSEARCH_H
+#endif   // BUTT_SEARCH_TAXSEARCH_H_
