bc005b46e0e73b4a2f2cb301774c03c2bd149336
Martin A. Hansen <mail@maasha.dk>
2015-08-10 16:10:14 +0200

    Added CleverTaxConsensus tests

--- BUTT/Tests/TestAll.cpp
+++ BUTT/Tests/TestAll.cpp
@@ -1,25 +1,29 @@
-#include "TestAll.h"
+/* Copyright 2015 BIO-DIKU */
 
+#include "TestAll.h"
 #include "TestFasta.h"
 #include "TestSeqToKMers.h"
 #include "TestTaxBuilder.h"
 #include "TestSimpleConsensus.h"
+#include "TestCleverTaxConsensus.h"
 #include "TestTaxSearch.h"
 #include "TestHelper.h"
 
 #include <iostream>
 
-TestAll::TestAll()
-{
+TestAll::TestAll() {
 }
 
-bool TestAll::runTests()
-{
+bool TestAll::runTests() {
     BUTT_PRE_TESTS();
-    BUTT_RUN_TEST("TestAll: TestFasta", TestFasta("../BUTT/Tests/Fastas").runTests());
+    BUTT_RUN_TEST("TestAll: TestFasta", TestFasta("../BUTT/Tests/Fastas").
+        runTests());
     BUTT_RUN_TEST("TestAll: TestSeqToKMers", TestSeqToKMers().runTests());
     BUTT_RUN_TEST("TestAll: TestTaxBuilder", TestTaxBuilder().runTests());
-    BUTT_RUN_TEST("TestAll: TestSimpleConsensus", TestSimpleConsensus().runTests());
+    BUTT_RUN_TEST("TestAll: TestSimpleConsensus", TestSimpleConsensus().
+        runTests());
+    BUTT_RUN_TEST("TestAll: TestCleverTaxConsensus", TestCleverTaxConsensus().
+        runTests());
     BUTT_RUN_TEST("TestAll: TestTaxSearch", TestTaxSearch().runTests());
     BUTT_POST_TESTS();
 }
