c578f4d10753b6d896cc28f82b4b3f12f43bc6b5
Martin Asser Hansen <mail@maasha.dk>
2015-08-18 13:07:38 +0200

    Rewrite ala cpplint

--- BUTT/Search/SimpleTaxConsensus.cpp
+++ BUTT/Search/SimpleTaxConsensus.cpp
@@ -1,3 +1,5 @@
+/* Copyright 2015 BIO-DIKU */
+
 #include "SimpleTaxConsensus.h"
 #include <map>
 #include <string>
@@ -6,38 +8,36 @@
 
 using namespace std;
 
-SimpleTaxConsensus::SimpleTaxConsensus(vector < string > _level_names)
-{
+SimpleTaxConsensus::SimpleTaxConsensus(vector < string > _level_names) {
     level_names = _level_names;
 }
 
-string SimpleTaxConsensus::buildConsensus(vector< vector< string > > &tax_table)
-{
-    int rows           = tax_table.size();
-    int level          = 0;
+string SimpleTaxConsensus::buildConsensus(vector< vector< string > > &tax_table) {
+    int          rows          = tax_table.size();
+    int          level         = 0;
     unsigned int min_row_length = 999999;
     string ret;
 
-    if(rows==0) return "";
+    if (rows == 0) return "";
 
-    for(int row=0;row<rows;++row){
+    for (int row = 0; row < rows; ++row) {
         if (tax_table[row].size() < min_row_length)
             min_row_length = tax_table[row].size();
     }
 
-    for(unsigned int col=0;col<min_row_length;++col){
+    for (unsigned int col=0; col < min_row_length; ++col) {
         if (columnPerfectConsensus(tax_table, col)) {
             if (tax_table[0][col][1] == '#') {
                 if (level > 0)
                     ret+=";";
 
                 level++;
+            } else {
+                ret += "_";
             }
-            else
-                ret+="_";
 
             ret+=tax_table[0][col];
-        } else {    
+        } else {
             break;
         }
     }
@@ -45,23 +45,24 @@ string SimpleTaxConsensus::buildConsensus(vector< vector< string > > &tax_table)
     return ret+buildTaxSuffix(level);
 }
 
-bool SimpleTaxConsensus::columnPerfectConsensus(vector< vector< string > > &tax_table, int col)
-{
-    for(unsigned int row=1;row<tax_table.size();row++){
-        if (tax_table[row][col] != tax_table[0][col])
+bool SimpleTaxConsensus::columnPerfectConsensus(vector< vector< string > > &tax_table, int col) {
+    for (unsigned int row = 1; row < tax_table.size(); row++) {
+        if (tax_table[row][col] != tax_table[0][col]) {
             return false;
+        }
     }
 
     return true;
 }
 
-string SimpleTaxConsensus::buildTaxSuffix(int level)
-{
+string SimpleTaxConsensus::buildTaxSuffix(int level) {
     string suffix = "";
 
-    for(unsigned int i=level;i < level_names.size(); ++i){
-        if (i > 0)
+    for (unsigned int i = level; i < level_names.size(); ++i) {
+        if (i > 0) {
             suffix += ";";
+        }
+
         suffix += level_names[i] + "#";
     }
 
